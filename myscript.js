9d428049d59a13d184f62751a01b2eb5bfc36199
davidemontagna <davidemontagna92@gmail.com>
2022-01-13 14:28:20 +0100

    Es. completato

---
+++ myscript.js
@@ -0,0 +1,8 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+      message: '"Luke! I am your father."',
+      colorTxt: 'dm-color-txt',
+      image: 'https://geekreply.com/wp-content/uploads/2017/06/empire-strikes-back.jpg'
+    }
+  })
